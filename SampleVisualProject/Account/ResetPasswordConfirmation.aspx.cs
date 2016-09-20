b7c5cdf7818760735896839edb9003b67ec2cd39
300915971 <300915971@PR-A313-14>
2016-09-19 21:32:58 -0400

    Add project files.

---
+++ SampleVisualProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SampleVisualProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SampleVisualProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleVisualProject.Startup))]
+namespace SampleVisualProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
